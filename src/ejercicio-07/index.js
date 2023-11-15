61daf19d55fd41b7a4a2f2ec452b5a1391de6cf7
Puri <purificacion.bastida@gmail.com>
2023-11-15 07:12:02 +0100

    Añadir ejer 7

--- src/ejercicio-07/index.js
+++ src/ejercicio-07/index.js
@@ -1,1 +1,13 @@
-// Escribe tu código aquí:
+const axios = require('axios')
+
+axios
+    .get(https://jsonplaceholder.typicode.com/posts/1')
+    .then((response) => {
+        console.log(response.data)
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+
+
+    
