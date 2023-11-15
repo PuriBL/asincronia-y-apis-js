cc2002cb9bdb465d1677e83474884797a92891ba
Puri <purificacion.bastida@gmail.com>
2023-11-15 23:33:14 +0100

    Añadir ejer 10

--- src/ejercicio-10/solucion/index.js
+++ src/ejercicio-10/solucion/index.js
@@ -5,3 +5,5 @@ const promise3 = new Promise((resolve) => setTimeout(() => resolve(3), 1500))
 Promise.all([promise1, promise2, promise3]).then((resultados) => {
   console.log('Todas las Promises se han resuelto:', resultados)
 })
+
+
