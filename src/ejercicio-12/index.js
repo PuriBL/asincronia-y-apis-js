b5c87e537a604cc26631995004e6217c64c9ed3b
Puri <purificacion.bastida@gmail.com>
2023-11-16 06:28:55 +0100

    Añadir ejer 12

--- src/ejercicio-12/index.js
+++ src/ejercicio-12/index.js
@@ -1,1 +1,11 @@
-// Escribe tu código aquí:
+async function procesarElementos(elementos) {
+    for (const elemento of elementos) {
+        await new Promise((resolve) => setTimeout(resolve, 1000))
+        console.log('Procesar elementos:', elemento)
+    }
+}
+
+const elementos = [1, 2, 3, 4, 5]
+procesarElementos(elementos)
+
+
